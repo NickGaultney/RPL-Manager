d91d17411a0ea2088140a5b3a83a07b52cb3aca3
Nick Mark <nmgaultney@gmail.com>
2020-08-30 23:17:26 -0500

    Bug Fix

--- app/controllers/api/v1/print_logs_api_controller.rb
+++ app/controllers/api/v1/print_logs_api_controller.rb
@@ -10,7 +10,14 @@ class Api::V1::PrintLogsApiController < ApplicationController
 	end
 
 	def create
-		@print_log = PrintLog.new(print_log_params)
+		log_params = print_log_params
+
+		params[:printer_id] = find_printer_by_name.id
+		convert_time_to_minutes
+		convert_weight_to_grams
+		params[:cost] = 0.0
+
+		@print_log = PrintLog.new(log_params)
 		if @print_log.save
 			render json: @print_log
 		else
@@ -26,7 +33,29 @@ class Api::V1::PrintLogsApiController < ApplicationController
 
 	private
 
+	def find_printer_by_name
+		@printer = Printer.find_by(name: params[:printer_name])
+	end
+
 	def print_log_params
 		params.permit(:printer_id, :printer_name, :file_name, :filament_weight, :print_time, :cost, :status)
+	end
+
+	def convert_time_to_minutes
+		minutes = 0
+		time = params[:print_time].split(/[\sa-zA-Z\s]+/)
+
+		# Convert hours to minutes
+		minutes += time[0].to_i * 60
+
+		# Add minutes
+		minutes += time[1].to_i
+
+		params[:print_time] = minutes.to_f
+	end
+
+	def convert_weight_to_grams
+		grams = params[:filament_weight].match(/\d+\.?\d*/)[0].to_f
+		params[:filament_weight] = grams
 	end
 end
