959571e336d9d24288f3971abdd6688ed11d4c33
Nick Mark <nmgaultney@gmail.com>
2020-09-03 09:12:28 -0500

    Calculate Cost

--- app/helpers/api/v1/print_logs_api_helper.rb
+++ app/helpers/api/v1/print_logs_api_helper.rb
@@ -1,15 +1,15 @@
 module Api::V1::PrintLogsApiHelper
-	def calculate_cost(weight, time)
-		COST_OF_PRINTER = 3000
-		COST_OF_FILAMENT = 23
-		COST_OF_KWH = 0.1608
-		TAZ_LIFE_EXPECTANCY = 4000		# Hours
-		COST_OF_WORKER = 13
-		TIME_PER_PART = 0.25			# Hours
-		TAZ_HEAT_UP_USAGE = 0.36		# KWH
-		TAZ_HEAT_UP_TIME = 0.25			# Hours
-		TAZ_STABLE_USAGE = 0.16			# KWH
+	COST_OF_PRINTER = 3000
+	COST_OF_FILAMENT = 23
+	COST_OF_KWH = 0.1608
+	TAZ_LIFE_EXPECTANCY = 4000		# Hours
+	COST_OF_WORKER = 13
+	TIME_PER_PART = 0.25			# Hours
+	TAZ_HEAT_UP_USAGE = 0.36		# KWH
+	TAZ_HEAT_UP_TIME = 0.25			# Hours
+	TAZ_STABLE_USAGE = 0.16			# KWH
 
+	def calculate_cost(weight, time)
 		total_printer = (COST_OF_PRINTER / TAZ_LIFE_EXPECTANCY.to_f) * (time / 60.0)
 		total_filament = (COST_OF_FILAMENT / (1000 / weight.to_f))
 		total_worker = (COST_OF_WORKER / TIME_PER_PART)
