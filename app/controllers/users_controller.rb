81bff2a767a09527d54ca93a47d00667a70cd91a
Nick Mark <nmgaultney@gmail.com>
2020-09-07 10:35:40 -0500

    Update User Form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,13 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		respond_to do |format|
-			if @printer.save
+			if @user.save
 	   			session[:user_id] = @user.id
 				format.html { redirect_to root_path, notice: 'Printer was successfully created.' }
-				format.json { render :show, status: :created, location: @printer }
+				format.json { render :show, status: :created, location: @user }
 			else
 				format.html { render :new }
-				format.json { render json: @printer.errors, status: :unprocessable_entity }
+				format.json { render json: @user.errors, status: :unprocessable_entity }
 			end
 		end
 	end
