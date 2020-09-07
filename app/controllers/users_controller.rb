a9f70d89a96c30f9ebc83585441cb232e423cfdd
Nick Mark <nmgaultney@gmail.com>
2020-09-07 10:07:29 -0500

    Add Register Link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
 class UsersController < ApplicationController
 	skip_before_action :authorized, only: [:new, :create]
-	
-	def new
-		@user = User.create(user_params)
-   		session[:user_id] = @user.id
-   		redirect_to root_path
+
+	def new	
 	end
 
 	def create
+		@user = User.new(user_params)
+		respond_to do |format|
+			if @printer.save
+	   			session[:user_id] = @user.id
+				format.html { redirect_to root_path, notice: 'Printer was successfully created.' }
+				format.json { render :show, status: :created, location: @printer }
+			else
+				format.html { render :new }
+				format.json { render json: @printer.errors, status: :unprocessable_entity }
+			end
+		end
 	end
 
 	private
