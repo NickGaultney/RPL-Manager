27f20375da4746c7bd5c79c0f0e3a8816e3c9b3f
Nick Mark <cbuprinting@gmail.com>
2020-09-11 17:09:02 +0100

    Add api_token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200627210644) do
+ActiveRecord::Schema.define(version: 20200911155929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 20200627210644) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "api_token"
   end
 
   add_foreign_key "print_logs", "printers"
