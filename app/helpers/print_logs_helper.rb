c9515cbd95819aa9901d1046ed803652d97218e8
Nick Mark <cbuprinting@gmail.com>
2020-10-01 14:17:39 +0100

    Refactor

--- app/helpers/print_logs_helper.rb
+++ app/helpers/print_logs_helper.rb
@@ -1,6 +1,9 @@
 module PrintLogsHelper
 	# Loops through all the PrintLogs on a given page
 	# and returns the sum of their costs
+	# *** NOTE ***
+	# This implementation could be more efficient if you 
+	# increment sum in the views.
 	def total_cost(print_logs)
 		cost = 0
 		print_logs.each do |log|
