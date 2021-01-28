508afd35ae9ae89f2a691a498225bec00335806a
Nick Mark <nmgaultney@gmail.com>
2021-01-28 08:58:20 -0800

    new home page

--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,6 @@ Rails.application.routes.draw do
     end
   end
 
-  root to: "printers#index"
+  root to: "statis#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
